7ef51a955e2c39facd43972a71f46a6fffeafa84
刘光强 <liuguangqiang@liuguangqiangdeMacBook-Pro.local>
2018-05-02 14:44:17 +0800

    初始化电子发票独立工程

---
+++ WLMElectronicInvoice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WLMElectronicInvoice
+//
+//  Created by 刘光强 on 2018/5/2.
+//  Copyright © 2018年 quangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WLMElectronicInvoice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WLMElectronicInvoice
+//
+//  Created by 刘光强 on 2018/5/2.
+//  Copyright © 2018年 quangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
