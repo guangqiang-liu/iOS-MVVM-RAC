0619c8a6ed87ee105127d772e4a8a242379043f0
刘光强 <liuguangqiang@qianbaocard.com>
2018-05-10 10:40:54 +0800

    添加自定义的环形进度条View

--- WLMElectronicInvoice/ViewController.m
+++ WLMElectronicInvoice/ViewController.m
@@ -11,6 +11,7 @@
 #import "WLMInvoiceManagerListVC.h"
 #import "WLMInvoiceSearchVC.h"
 #import "WLMInvoiceDetailVC.h"
+#import "WLCircleProgressView.h"
 
 #import "WLMPackageSelectVC.h"
 #define ktag 1000
@@ -24,10 +25,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    WLCircleProgressView *progressView = [[WLCircleProgressView alloc] initWithFrame:CGRectMake(100, 100, 150, 150) circleWidth:10 gradientCGColors:@[(id)HexRGB(0x72D7FF).CGColor, (id)HexRGB(0x4B77FF).CGColor]];
+    [self.view addSubview:progressView];
+    progressView.progress = @(0.8);
+    
     NSArray *array = @[@"电票申请",@"电票管理",@"电票搜索",@"电票详情"];
     for (NSInteger i = 0; i < 4; i ++) {
         UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-        button.frame = CGRectMake(100, 100 + i * 60, 100, 40);
+        button.frame = CGRectMake(300, 100 + i * 60, 100, 40);
         button.tag = ktag + i;
         [button setTitle:array[i] forState:UIControlStateNormal];
         [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
@@ -35,6 +41,7 @@
         [self.view addSubview:button];
     }
     
+    // UIButton使用方式
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
     [btn setTitle:@"字体图片测试" forState:UIControlStateNormal];
     btn.frame = CGRectMake(0, 400, 200, 30);
@@ -58,7 +65,7 @@
 - (void)btnClick:(UIButton *)button {
     NSInteger tag = button.tag - ktag;
     if (tag == 0) {
-        WLMPackageSelectVC *VC = [[WLMPackageSelectVC alloc] init];
+        WLMInvoiceApplyVC *VC = [[WLMInvoiceApplyVC alloc] init];
         [self.navigationController pushViewController:VC animated:YES];
     } else if (tag == 1) {
         WLMInvoiceManagerListVC *VC = [[WLMInvoiceManagerListVC alloc] init];
